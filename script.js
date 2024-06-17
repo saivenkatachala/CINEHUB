c5858d2bbca9c0139fc6274e6fe2cf948d6397db
A saivenkat <110588670+saivenkatachala@users.noreply.github.com>
2024-06-17 18:26:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const movies = [
+        {
+            title: 'Gangs of Godavari (2024)Telugu ',
+            poster: 'poster1.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314224?hash=AgADJR',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314222?hash=AgADKB',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314224?hash=AgADJR',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314238?hash=AgADIh'
+            }
+        },
+        {
+            title: 'Love Me if You Dare (2024) Telugu',
+            poster: 'poster2.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314260?hash=AgADyh',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314262?hash=AgADOh',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314260?hash=AgADyh',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314258?hash=AgADwh'
+            }
+
+        },
+        {
+
+            title: 'Dear Nanna (2024) Telugu',
+            poster: 'poster3.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314312?hash=AgADMh',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314312?hash=AgADMh',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314314?hash=AgADOx',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314310?hash=AgADwB'
+            }
+        },
+        {
+            title: 'Parijatha Parvam (2024) Telugu ',
+            poster: 'poster4.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314334?hash=AgADbA',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314334?hash=AgADbA',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314332?hash=AgADaQ',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314330?hash=AgADZQ'
+            }
+
+        },
+        {
+            title: 'Star (2024) Telugu ',
+            poster: 'poster5.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314344?hash=AgADmg',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314342?hash=AgADnQ',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314344?hash=AgADmg',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314340?hash=AgADmA'
+            }
+
+        },
+        {
+            title: 'Blackout (2024)[Tel+Hin+Kan+Tam] ',
+            poster: 'poster6.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314374?hash=AgADBh',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314376?hash=AgADCR',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314374?hash=AgADBh',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314372?hash=AgADBx'
+            }
+
+        },
+        {
+            title: 'Tenant (2024) Telugu  ',
+            poster: 'poster7.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314390?hash=AgADnw',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314390?hash=AgADnw',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314392?hash=AgADqA',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314388?hash=AgADqw'
+            }
+
+        },
+        {
+            title: 'Kalki (2024) Telugu',
+            poster: 'poster8.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314409?hash=AgADbB',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314407?hash=AgAD0h',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314409?hash=AgADbB',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314405?hash=AgADUh'
+            }
+
+        },
+        {
+            title: 'Varshangalkku Shesham (2024)[Tel + Hin + Kan]   ',
+            poster: 'poster9.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314415?hash=AgADYB',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314419?hash=AgADoR',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314415?hash=AgADYB',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314417?hash=AgADhR'
+            }
+
+        },
+        {
+            title: 'Bade Miyan Chote Miyan (2024)[Tel + Mal + Kan] ',
+            poster: 'poster10.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314429?hash=AgADZx',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314427?hash=AgADtR',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314429?hash=AgADZx',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314425?hash=AgADVh'
+            }
+
+        },
+        {
+            title: 'Naadu (2023) [Tel + Mal]  ',
+            poster: 'poster11.jpg',
+            watchLink: 'https://tgmstreamm-cc23fd301808.herokuapp.com/watch/314471?hash=AgADGw',
+            downloadLinks: {
+                '1080p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314469?hash=AgADIA',
+                '720p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314471?hash=AgADGw',
+                '480p': 'https://tgmstreamm-cc23fd301808.herokuapp.com/314467?hash=AgADGA'
+            }
+
+        }
+        // Add more movies here
+    ];
+
+    const movieList = document.getElementById('movie-list');
+    
+    function displayMovies(movieArray) {
+        movieList.innerHTML = '';
+        movieArray.forEach(movie => {
+            const movieElement = document.createElement('div');
+            movieElement.className = 'movie';
+            movieElement.innerHTML = `
+                <img src="${movie.poster}" alt="${movie.title}">
+                <div class="movie-content">
+                    <h2>${movie.title}</h2>
+                    <a href="${movie.watchLink}" target="_blank" class="button">Watch Online</a>
+                    <div class="download-options">
+                        <a href="${movie.downloadLinks['1080p']}" download class="button">Download 1080p</a>
+                        <a href="${movie.downloadLinks['720p']}" download class="button">Download 720p</a>
+                        <a href="${movie.downloadLinks['480p']}" download class="button">Download 480p</a>
+                    </div>
+                </div>
+            `;
+            movieList.appendChild(movieElement);
+        });
+    }
+
+    function searchMovies() {
+        const query = document.getElementById('search-input').value.toLowerCase();
+        const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(query));
+        displayMovies(filteredMovies);
+    }
+
+    displayMovies(movies);
+    window.searchMovies = searchMovies;
+});
---
+++ series-script.js
@@ -0,0 +1,147 @@
+const seriesData = [
+    {
+        title: "B & B Bujji and Bhairava",
+        image: "series1.jpg",
+        description: "Language:Telugu,Hindi,English",
+        seasons: [
+            {
+                season: "Season 1",
+                episodes: [
+                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/325953?hash=AgADSQ", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/325953?hash=AgADSQ" },
+                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/325955?hash=AgADTA", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/325955?hash=AgADTA" }
+                ]
+            },
+            
+        ]
+    },
+    {
+        title: "Gullak 1",
+        image: "G12.avif",
+        description: " [ Hin+Kan+Mal+Tam+Tel]-Esub",
+        seasons: [
+            {
+                season: "Season 1",
+                episodes: [
+                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328245?hash=AgADrx", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328245?hash=AgADrx" },
+                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328265?hash=AgADsB", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328265?hash=AgADsB" },
+                    { title: "Episode 3", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328256?hash=AgADsR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328256?hash=AgADsR" },
+                    { title: "Episode 4", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328251?hash=AgADsx",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328251?hash=AgADsx" },
+                    { title: "Episode 5", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328247?hash=AgADtR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328247?hash=AgADtR" }
+                ]
+            }
+        ]
+    },
+    {
+        title: "Gullak 2",
+        image: "G23.jpg",
+        description: " [ Hin+Kan+Mal+Tam+Tel]-Esub",
+        seasons: [
+            {
+                season: "Season 2",
+                episodes: [
+                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328297?hash=AgADuh", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328297?hash=AgADuh" },
+                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328302?hash=AgADux", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328302?hash=AgADux" },
+                    { title: "Episode 3", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328291?hash=AgADvB",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328291?hash=AgADvB" },
+                    { title: "Episode 4", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328309?hash=AgADvh",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328309?hash=AgADvh" },
+                    { title: "Episode 5", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328293?hash=AgADvx",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328293?hash=AgADvx" }
+                ]
+            }
+        ]
+    },
+    {
+    title: "Gullak 3",
+        image: "G3.avif",
+        description: " [ Hin+Kan+Mal+Tam+Tel]-Esub",
+        seasons: [
+            {
+                season: "Season 3",
+                episodes: [
+                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328311?hash=AgADjx", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328311?hash=AgADjx" },
+                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328319?hash=AgADjB", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328319?hash=AgADjB" },
+                    { title: "Episode 3", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328314?hash=AgADlB",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328314?hash=AgADlB" },
+                    { title: "Episode 4", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328328?hash=AgADmR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328328?hash=AgADmR" },
+                    { title: "Episode 5", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328313?hash=AgADlR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328313?hash=AgADlR" }
+                ]
+            }
+        ]
+    },
+    {
+        title: "Gullak 4 ",
+        image: "G4.webp",
+        description: " [ Hin+Kan+Mal+Tam+Tel]-Esub",
+        seasons: [
+            {
+                season: "Season 4",
+                episodes: [
+                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328331?hash=AgADix", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328331?hash=AgADix" },
+                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328334?hash=AgADjB", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328334?hash=AgADjB" },
+                    { title: "Episode 3", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328339?hash=AgADkB",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328339?hash=AgADkB" },
+                    { title: "Episode 4", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328337?hash=AgADjR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328337?hash=AgADjR" },
+                    { title: "Episode 5", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328333?hash=AgADjx",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328333?hash=AgADjx" }
+                ]
+            }
+        ]
+    }
+    
+
+    // Add more series as needed
+];
+
+function createEpisodeElement(episode) {
+    return `
+        <h4>${episode.title}</h4>
+        <a href="${episode.watchLink}" class="button">Watch Online</a>
+        <a href="${episode.downloadLink}" class="button">Download</a>
+    `;
+}
+
+function createSeasonElement(season) {
+    return `
+        <div class="season">
+            <h3>${season.season}</h3>
+            <div class="episode-list">
+                ${season.episodes.map(createEpisodeElement).join('')}
+            </div>
+        </div>
+    `;
+}
+
+function createSeriesElement(series) {
+    const seriesElement = document.createElement("div");
+    seriesElement.classList.add("series");
+
+    seriesElement.innerHTML = `
+        <img src="${series.image}" alt="${series.title}">
+        <div class="series-content">
+            <h2>${series.title}</h2>
+            <p>${series.description}</p>
+            ${series.seasons.map(createSeasonElement).join('')}
+        </div>
+    `;
+
+    return seriesElement;
+}
+
+function displaySeries() {
+    const seriesList = document.getElementById("series-list");
+    seriesList.innerHTML = "";
+    seriesData.forEach(series => {
+        const seriesElement = createSeriesElement(series);
+        seriesList.appendChild(seriesElement);
+    });
+}
+
+function searchSeries() {
+    const query = document.getElementById("search-input-series").value.toLowerCase();
+    const filteredSeries = seriesData.filter(series => series.title.toLowerCase().includes(query));
+    
+    const seriesList = document.getElementById("series-list");
+    seriesList.innerHTML = "";
+    filteredSeries.forEach(series => {
+        const seriesElement = createSeriesElement(series);
+        seriesList.appendChild(seriesElement);
+    });
+}
+
+// Display series on page load
+window.onload = displaySeries;
