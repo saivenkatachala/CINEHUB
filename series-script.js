ed4cd82d1b6e3d6818f2038e6dc1151e1fe516f9
A saivenkat <110588670+saivenkatachala@users.noreply.github.com>
2025-01-30 15:29:14 +0530

    Update series-script.js

--- series-script.js
+++ series-script.js
@@ -7,131 +7,14 @@ const seriesData = [
             {
                 season: "Season 1",
                 episodes: [
-                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/216670?hash=AgADsh", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/216670?hash=AgADsh" },
-                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/216672?hash=AgADrh", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/216672?hash=AgADrh" }
+                    { title: "Episode 1", watchLink: "", downloadLink: "" },
+                    { title: "Episode 2", watchLink: "", downloadLink: "" }
                 ]
             },
             
         ]
     },
-     {
-        title: "Mirzapur (2020) S02 EP (01-10)",
-        image: "series11.jpg",
-        description: "Language:Telugu,Hindi,English",
-        seasons: [
-            {
-                season: "Season 2",
-                episodes: [
-                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/216688?hash=AgADtx", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/216688?hash=AgADtx" },
-                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/216686?hash=AgADsR", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/216686?hash=AgADsR" }
-                ]
-            },
-            
-        ]
-    },
-    {
-        title: "Mirzapur (2020) S02 EP (01-10)",
-        image: "series12.jpg",
-        description: "Language:Telugu,Hindi,English",
-        seasons: [
-            {
-                season: "Season 3",
-                episodes: [
-                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/216704?hash=AgADEx", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/216704?hash=AgADEx" },
-                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/216702?hash=AgADTw", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/216702?hash=AgADTw" }
-                ]
-            },
-            
-        ]
-    },
-    {
-        title: "B & B Bujji and Bhairava",
-        image: "series1.jpg",
-        description: "Language:Telugu,Hindi,English",
-        seasons: [
-            {
-                season: "Season 1",
-                episodes: [
-                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/325953?hash=AgADSQ", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/325953?hash=AgADSQ" },
-                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/325955?hash=AgADTA", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/325955?hash=AgADTA" }
-                ]
-            },
-            
-        ]
-    },
-    {
-        title: "Gullak 1",
-        image: "G12.avif",
-        description: " [ Hin+Kan+Mal+Tam+Tel]-Esub",
-        seasons: [
-            {
-                season: "Season 1",
-                episodes: [
-                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328245?hash=AgADrx", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328245?hash=AgADrx" },
-                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328265?hash=AgADsB", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328265?hash=AgADsB" },
-                    { title: "Episode 3", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328256?hash=AgADsR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328256?hash=AgADsR" },
-                    { title: "Episode 4", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328251?hash=AgADsx",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328251?hash=AgADsx" },
-                    { title: "Episode 5", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328247?hash=AgADtR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328247?hash=AgADtR" }
-                ]
-            }
-        ]
-    },
-    {
-        title: "Gullak 2",
-        image: "G23.jpg",
-        description: " [ Hin+Kan+Mal+Tam+Tel]-Esub",
-        seasons: [
-            {
-                season: "Season 2",
-                episodes: [
-                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328297?hash=AgADuh", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328297?hash=AgADuh" },
-                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328302?hash=AgADux", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328302?hash=AgADux" },
-                    { title: "Episode 3", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328291?hash=AgADvB",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328291?hash=AgADvB" },
-                    { title: "Episode 4", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328309?hash=AgADvh",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328309?hash=AgADvh" },
-                    { title: "Episode 5", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328293?hash=AgADvx",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328293?hash=AgADvx" }
-                ]
-            }
-        ]
-    },
-    {
-    title: "Gullak 3",
-        image: "G3.avif",
-        description: " [ Hin+Kan+Mal+Tam+Tel]-Esub",
-        seasons: [
-            {
-                season: "Season 3",
-                episodes: [
-                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328311?hash=AgADjx", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328311?hash=AgADjx" },
-                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328319?hash=AgADjB", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328319?hash=AgADjB" },
-                    { title: "Episode 3", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328314?hash=AgADlB",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328314?hash=AgADlB" },
-                    { title: "Episode 4", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328328?hash=AgADmR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328328?hash=AgADmR" },
-                    { title: "Episode 5", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328313?hash=AgADlR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328313?hash=AgADlR" }
-                ]
-            }
-        ]
-    },
-    {
-        title: "Gullak 4 ",
-        image: "G4.webp",
-        description: " [ Hin+Kan+Mal+Tam+Tel]-Esub",
-        seasons: [
-            {
-                season: "Season 4",
-                episodes: [
-                    { title: "Episode 1", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328331?hash=AgADix", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328331?hash=AgADix" },
-                    { title: "Episode 2", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328334?hash=AgADjB", downloadLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/328334?hash=AgADjB" },
-                    { title: "Episode 3", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328339?hash=AgADkB",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328339?hash=AgADkB" },
-                    { title: "Episode 4", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328337?hash=AgADjR",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328337?hash=AgADjR" },
-                    { title: "Episode 5", watchLink: "https://tgmstreamm-cc23fd301808.herokuapp.com/watch/328333?hash=AgADjx",downloadLink:"https://tgmstreamm-cc23fd301808.herokuapp.com/328333?hash=AgADjx" }
-                ]
-            }
-        ]
-    }
-    
-
-    // Add more series as needed
-];
-
+    };
 function createEpisodeElement(episode) {
     return `
         <h4>${episode.title}</h4>
